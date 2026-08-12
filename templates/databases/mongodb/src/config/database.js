296a1f61615fbe6c27f6df3627585f023b784e50
shahidansari311 <shahidansari945256@gmail.com>
2026-08-12 19:03:37 +0530

    refactor: update database connection logic and improve Prisma client integration

--- templates/databases/mongodb/src/config/database.js
+++ templates/databases/mongodb/src/config/database.js
@@ -11,4 +11,6 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+module.exports = {
+  connectDB
+};
--- templates/databases/postgresql/src/config/database.js
+++ templates/databases/postgresql/src/config/database.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 
-const { PrismaClient } = require("../generated/prisma");
+const { PrismaClient } = require("../generated/prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 const connectDB = async () => {
   try {
